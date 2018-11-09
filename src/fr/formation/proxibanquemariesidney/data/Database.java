831531dad27a4c44bbce8c33656c3192423d32ae
Marie <marie.boulez@laposte.net>
2018-11-09 11:37:35 +0100

    Modif récupération conseiller et client choisis.

--- src/fr/formation/proxibanquemariesidney/data/Database.java
+++ src/fr/formation/proxibanquemariesidney/data/Database.java
@@ -3,9 +3,7 @@ package fr.formation.proxibanquemariesidney.data;
 import java.util.ArrayList;
 import java.util.List;
 
-import fr.formation.proxibanquemariesidney.business.Advisor;
 import fr.formation.proxibanquemariesidney.business.Branch;
-import fr.formation.proxibanquemariesidney.business.Client;
 
 public class Database {
 	
@@ -14,30 +12,5 @@ public class Database {
 	public Database () {
 		this.branchList = new ArrayList<>();
 	}
-	
-	public Advisor getAdvisor1() {
-		return this.branchList.get(0).advisorList.get(0);
-	}
-	
-	public Advisor getAdvisor2() {
-		return this.branchList.get(0).advisorList.get(1);
-	}
-	
-	public Client getClient11() {
-			return this.branchList.get(0).advisorList.get(0).clientList.get(0);
-	}
-	
-	public Client getClient12() {
-		return this.branchList.get(0).advisorList.get(0).clientList.get(1);
-	}
-	
-	public Client getClient21() {
-		return this.branchList.get(0).advisorList.get(1).clientList.get(0);
-	}
-	
-	public Client getClient22() {
-		return this.branchList.get(0).advisorList.get(1).clientList.get(1);
-	}
-
 
 }
